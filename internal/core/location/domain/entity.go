0ba71eb120ccc00b8b03ceb50ba1aa8391eae19b
SevgiF <svg.fdn00@gmail.com>
2025-06-02 14:35:35 +0300

    fix: struct value updated

--- internal/core/location/domain/entity.go
+++ internal/core/location/domain/entity.go
@@ -9,7 +9,7 @@ type Location struct {
 	Name        string    `json:"name" gorm:"type:varchar(128);not null" validate:"required,min=3,max=128"`
 	Latitude    float64   `json:"latitude" gorm:"type:decimal(10,8);not null" validate:"required"`
 	Longitude   float64   `json:"longitude" gorm:"type:decimal(11,8);not null" validate:"required"`
-	MarkerColor string    `json:"markerColor" gorm:"type:varchar(6);not null" validate:"required,hexadecimal"`
+	MarkerColor string    `json:"marker_color" gorm:"type:varchar(6);not null" validate:"required,hexadecimal"`
 	CreatedAt   time.Time `json:"created_at"`
 	UpdatedAt   time.Time `json:"updated_at"`
 }
